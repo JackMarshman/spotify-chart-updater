716d6a80e1666a316510b7d515651a32ec12c05f
Marshman <jack.marshman@outlook.com>
2023-05-11 19:33:00 +0100

    Initial commit

---
+++ src/main/java/spotifychartupdater/SpotifyChartUpdaterApplication.java
@@ -0,0 +1,13 @@
+package spotifychartupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyChartUpdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyChartUpdaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spotifychartupdater/SpotifyChartUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package spotifychartupdater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyChartUpdaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
